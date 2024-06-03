926dccbf5fa9ba77e835687d4856fbffdf1eeed2
josuerx12 <josueaze12@gmail.com>
2024-06-03 09:00:14 -0300

    feat: NEW FEATURES

--- src/utils/s3Auth.js
+++ src/utils/s3Auth.js
@@ -2,8 +2,8 @@ const { S3Client } = require("@aws-sdk/client-s3");
 const s3 = new S3Client({
   region: "us-east-2",
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.ACCESS_KEY,
+    secretAccessKey: process.env.SECRET_KEY,
   },
 });
 
